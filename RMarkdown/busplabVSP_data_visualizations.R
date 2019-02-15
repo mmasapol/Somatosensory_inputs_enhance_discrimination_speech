39b935ccf29d535fedff4dab67bf4ad9be596fda
mmasapol <35698151+mmasapol@users.noreply.github.com>
2019-02-15 11:07:42 -0500

    Add files via upload

--- RMarkdown/busplabVSP_data_visualizations.R
+++ RMarkdown/busplabVSP_data_visualizations.R
@@ -27,7 +27,7 @@ Experiment1_dynamic_speech %>%
   ylim(0.5,1.0) +
   ylab("Perceptual Sensitivity (A')") +
   xlab("Experimental Condition") + 
-  scale_x_discrete(labels=c("baseline" = "Baseline", "bite_block" = "Block\nBite", "lip_tube" = "Lip\nTube")) +
+  scale_x_discrete(labels=c("baseline" = "Baseline", "bite_block" = "Bite\nBlock", "lip_tube" = "Lip\nTube")) +
   theme(axis.text=element_text(size=20),
         axis.title=element_text(size=24,face="bold")) +
   theme(legend.text=element_text(size=20),
@@ -56,7 +56,7 @@ Experiment2_static_speech %>%
   ylim(0.5,1.0) +
   ylab("Perceptual Sensitivity (A')") +
   xlab("Experimental Condition") + 
-  scale_x_discrete(labels=c("baseline" = "Baseline", "bite_block" = "Block\nBite", "lip_tube" = "Lip\nTube")) +
+  scale_x_discrete(labels=c("baseline" = "Baseline", "bite_block" = "Bite\nBlock", "lip_tube" = "Lip\nTube")) +
   theme(axis.text=element_text(size=20),
         axis.title=element_text(size=24,face="bold")) +
   theme(legend.text=element_text(size=20),
@@ -85,7 +85,79 @@ Experiment3_staircase %>%
   ylim(0,70) +
   ylab("JND (step size)") +
   xlab("Experimental Condition") + 
-  scale_x_discrete(labels=c("C" = "Baseline", "BB" = "Block\nBite", "LT" = "Lip\nTube")) +
+  scale_x_discrete(labels=c("C" = "Baseline", "BB" = "Bite\nBlock", "LT" = "Lip\nTube")) +
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=24,face="bold")) +
+  theme(legend.text=element_text(size=20),
+        legend.title = element_text(size=24))
+
+
+## Experiment 3 (All data)
+## Load data frame
+Experiment3_staircase <- read_xlsx(file.choose())
+
+## View data frame
+View(Experiment3_staircase)
+
+Experiment3_staircase$run <- NULL
+
+Experiment3_staircase$auditory_series <- revalue(x = Experiment3_staircase$auditory_series, c("En_u_Fr_u" = "English /u/ - French /u/", "E_AE" = "English /ɛ-æ/"))
+
+Experiment3_staircase$group <- revalue(x = Experiment3_staircase$group, c("bite_block" = "Bite Block", "lip_tube" = "Lip Tube"))
+
+Experiment3_staircase$condition <- revalue(x = Experiment3_staircase$condition, c("NoPert" = "No Pert", "Pert" = "Pert"))
+
+## Experiment3_staircase$subject <- factor(Experiment3_staircase$subject)
+
+Experiment3_staircase %>%
+  ggplot() + geom_boxplot(aes(condition,jnd),notch=FALSE, outlier.shape = NA) +
+  facet_grid(vars(group), vars(auditory_series)) +
+  ## facet_wrap(~subject) +
+  theme(strip.text = element_text(face="bold", size = 20)) +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 16),
+        legend.title = element_text(size = 18),
+        legend.text = element_text(size = 18)) +
+  ylim(0,70) +
+  ylab("JND (step size)") +
+  xlab("Experimental Condition") + 
+  scale_x_discrete(labels=c("baseline" = "Baseline", "bite_block" = "Bite\nBlock", "lip_tube" = "Lip\nTube")) +
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=24,face="bold")) +
+  theme(legend.text=element_text(size=20),
+        legend.title = element_text(size=24))
+
+
+## Experiment 3 (First two blocks in each condition only, i.e., runs 1-2, 5-6)
+## Load data frame
+Experiment3_staircase <- read_xlsx(file.choose())
+
+## View data frame
+View(Experiment3_staircase)
+
+Exp3_first_block_only <- Experiment3_staircase %>%
+  filter(run %in% c("1", "2", "5","6"))
+
+View(Exp3_first_block_only)
+
+Exp3_first_block_only$auditory_series <- revalue(x = Exp3_first_block_only$auditory_series, c("En_u_Fr_u" = "English /u/ - French /u/", "E_AE" = "English /ɛ-æ/"))
+
+Exp3_first_block_only$group <- revalue(x = Exp3_first_block_only$group, c("bite_block" = "Bite Block", "lip_tube" = "Lip Tube"))
+
+Exp3_first_block_only$condition <- revalue(x = Exp3_first_block_only$condition, c("NoPert" = "No Pert", "Pert" = "Pert"))
+
+Exp3_first_block_only %>%
+  ggplot() + geom_boxplot(aes(condition,jnd),notch=FALSE, outlier.shape = NA) +
+  facet_grid(vars(group), vars(auditory_series)) +
+  theme(strip.text = element_text(face="bold", size = 20)) +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 16),
+        legend.title = element_text(size = 18),
+        legend.text = element_text(size = 18)) +
+  ylim(0,70) +
+  ylab("JND (step size)") +
+  xlab("Experimental Condition") + 
+  scale_x_discrete(labels=c("baseline" = "Baseline", "bite_block" = "Bite\nBlock", "lip_tube" = "Lip\nTube")) +
   theme(axis.text=element_text(size=20),
         axis.title=element_text(size=24,face="bold")) +
   theme(legend.text=element_text(size=20),
